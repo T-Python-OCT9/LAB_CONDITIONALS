a9d826feea3557b949b8a6f77f42126a9da7a7a4
Razan Alshehri <alshehri.razan8@gmail.com>
2022-10-11 17:27:50 +0300

    lab 3

---
+++ Movies.py
@@ -0,0 +1,12 @@
+Movie : str = "Seven"
+Rate : int = 3
+score : float = 72.65
+
+if Rate >= 4 and score > 80 :
+     print ("highly Recommanded")
+elif Rate >= 3 and score > 70 :
+    print ("I recommended it . It is good")
+elif Rate >= 2 and score > 60 :
+    print ("You should check it out!")
+else:
+        print ("Don't watch it. It is a waste of time")
