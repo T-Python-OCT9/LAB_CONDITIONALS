6bd4494fde47d9fc2c975345532c784e72dc9dd2
Razan.abdullah <rrazanabdullahh@gmail.com>
2022-10-11 19:09:37 +0300

    solved lab for conditionals

---
+++ main.py
@@ -0,0 +1,19 @@
+
+
+movie : str = "Luck"
+rating : int = 3
+score : float = 72.65
+
+
+
+if rating >= 4 and score > 80:
+    print("Highly recommended")
+elif rating >= 3 and score > 70:
+    print("I recommended it . It is good")
+elif rating <= 2 and score > 60:
+    print("You should check it out!!")
+elif rating <= 2 and score > 50:
+    print("Don't watch it. It is a waste of time")
+else:
+    print("Exit")
+    
