6ddadf7b500170891b4a71d0f8b3c2a76cceeb30
Bahiyyah <70109610+bahiyyah-4@users.noreply.github.com>
2022-10-11 17:25:37 +0300

    Update lab_conditionals.py

--- lab_conditionals.py
+++ lab_conditionals.py
@@ -5,7 +5,7 @@ if rating <= 4 and popularity < 80:
     print("Highly recommended")
 elif rating <= 3 or popularity < 70:
     print("I recommended it . It is good")
-elif rating >= 2 or popularity < 60:
+elif rating >= 2 or popularity > 60:
     print("You should check it out!")
 else:
     print("Don't watch it. It is a waste of time")
